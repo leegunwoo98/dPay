bf960c0502d0fadd58c47ed385e3c542ca1d4926
Gunwoo Lee <gunwoo@wustl.edu>
2022-10-14 20:43:06 -0500

    hello:

--- client/Login.js
+++ client/Login.js
@@ -1,3 +1,14 @@
-function login_page(){
-    <View
+import React from "react"
+import { StyleSheet, Text, View, TextInput } from "react-native"
+import styles from "./StyleSheet"
+const Login = () => {
+    const [username, setUsername] = React.useState("")
+    const [password, setPassword] = React.useState("")
+    return (
+    <View   style={styles.container}>
+        <TextInput style={styles.input} onChangeText ={setUsername} value={username}></TextInput>
+        <TextInput style={styles.input} onChangeText ={setPassword} value={password}></TextInput>
+    </View>
+    )
 }
+export default Login
