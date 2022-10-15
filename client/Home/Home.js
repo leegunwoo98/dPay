6c9689f8e5eaf2b37afdccf93f8cdbdfa9c3b17d
Gunwoo Lee <gunwoo@wustl.edu>
2022-10-15 01:46:13 -0500

    initialized home

--- client/Home/Home.js
+++ client/Home/Home.js
@@ -2,16 +2,34 @@ import React from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 import styles from "./HomeStyleSheet.js";
 
-export default function Home({ userID, setPageID }) {
+export default function Home({ user, setPageID }) {
+    const [user,setUser]=React.useState(null);
+    const [transaction, setTransaction] = React.useState(null);
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Home</Text>
-            <Text style={styles.text}>User ID: {userID}</Text>
-            <Button
-                title="Logout"
-                style={styles.button}
-                onPress={() => setPageID("login")}
-            ></Button>
+            <View style = {styles.top}>
+                <View style={styles.logOut_container}>
+                    <Button
+                        title="Log Out"
+                        style={styles.logOut}
+                        onPress={() => setPageID("login")}
+                    ></Button>
+                </View>
+                <View style={styles.userID_container}>
+                    <Text style={styles.userID}>User ID: {userID}</Text>
+                </View>
+                <View style={style.settings_container}>
+                    <Button
+                        title="Settings"
+                        style={styles.settings}
+                        onPress={() => setPageID("settings")}
+                    ></Button>
+                </View>
+            </View>
+            <View style={styles.balance_container}>
+                <Text style={styles.balance}>Balance: $0.00</Text>
+            </View>
+
         </View>
     );
 }
